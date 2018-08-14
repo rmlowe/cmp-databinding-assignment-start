622623ae79c52cf67b5b1c5d71442095046bc60c
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-14 23:57:33 +0800

    Assignment 4: Practicing Property & Event Binding and View Encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  emittedEvents: {isEven: boolean, num: number}[] = [];
+
+  onEventEmitted(num: number) {
+    this.emittedEvents.push({isEven: num % 2 === 0, num});
+    console.log('Event emitted: ' + num);
+  }
 }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() eventEmitted = new EventEmitter<number>();
+  ref: number;
+  num = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartGame() {
+    console.log('Game started!');
+    if (!this.ref) {
+      this.ref = window.setInterval(() => {
+        this.eventEmitted.emit(this.num++);
+      }, 1000);
+    }
+  }
+
+  onStopGame() {
+    console.log('Game stopped!');
+    if (this.ref) {
+      window.clearInterval(this.ref);
+      this.ref = undefined;
+    }
   }
 
 }
